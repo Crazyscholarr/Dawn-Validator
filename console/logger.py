e170562a9c9c12f958527ecbb33b7cd3f6b85029
Crazyscholarr <nguyenlephuoctrong1502@gmail.com>
2025-02-14 09:55:48 +0700

    Update logger.py

--- console/logger.py
+++ console/logger.py
@@ -2,12 +2,12 @@ from colorama import Fore
 
 
 def error_log(message: str):
-    print(Fore.RED + ">> ERROR |" + Fore.LIGHTBLACK_EX + f" {message}")
+    print(Fore.RED + ">> Crazyscholar |" + Fore.LIGHTBLACK_EX + f" {message}")
 
 
 def success_log(message: str):
-    print(Fore.GREEN + ">> SUCCESS |" + Fore.LIGHTBLACK_EX + f" {message}")
+    print(Fore.GREEN + ">> Crazyscholar |" + Fore.LIGHTBLACK_EX + f" {message}")
 
 
 def info_log(message: str):
-    print(Fore.LIGHTBLACK_EX + f">> INFO | {message}")
+    print(Fore.LIGHTBLACK_EX + f">> Crazyscholar | {message}")
